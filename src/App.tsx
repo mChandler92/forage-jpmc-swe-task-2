2518e99b66edb51255287f4bf231f3fa95b43b78
Matt Chandler <142733187+TherpyDude@users.noreply.github.com>
2024-04-25 14:52:46 -0500

    Installed environment dependencies. Edited Graph.tsx and App.tsx files to enable appropriate DOM manipulation and ouput for data stream.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  // added additional required property to visually display graph upon render app component render
+  showGraph: boolean,
 }
 
 /**
@@ -22,6 +24,8 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      // added default and initial state for newly added property in constructor 
+      showGraph: false,
     };
   }
 
@@ -29,18 +33,32 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    // inserted if statement to ensure that a graph is rendered if and only if the state of showGraph is true (which occurs on click)
+    if (this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
+    let x = 0;
+    const interval = setInterval(() => {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
+      this.setState({
+        // add showGraph state to method (triggered onClick). Edited data setState for data 
+        data: serverResponds,
+        showGraph: true,
+      });
     });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);
   }
 
   /**
